7524a8016144ff1426a900fc626875f815dc7a4c
aryanteng <aryanteng892@gmail.com>
2023-05-23 23:21:44 +0530

    advanced routing

--- routes/users.js
+++ routes/users.js
@@ -13,8 +13,24 @@ router.post('/', (req, res) => {
   res.send('create user');
 });
 
-router.get('/:id', (req, res) => {
-  res.send(`user id: ${req.params.id}`);
+router
+  .route('/:id')
+  .get((req, res) => {
+    console.log(req.user);
+    res.send(`get user id: ${req.params.id}`);
+  })
+  .put((req, res) => {
+    res.send(`udpate user id: ${req.params.id}`);
+  })
+  .delete((req, res) => {
+    res.send(`delete user id: ${req.params.id}`);
+  });
+
+const user = [{ name: 'Aryan' }, { name: 'Avi' }];
+
+router.param('id', (req, res, next, id) => {
+  req.user = user[id];
+  next();
 });
 
 module.exports = router;
